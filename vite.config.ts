6ff08c3977ced3d2d7848c7304e39cd73992d7ea
Ehhthan <ethanhaynes46@gmail.com>
2025-08-01 11:19:44 -0500

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -14,14 +14,15 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: path.resolve(__dirname, "src/main.tsx"),
-                bookDisplayWidget: path.resolve(__dirname, "src/book-display/book-display-widget.tsx"),
+                bookDisplayWidget: path.resolve(__dirname, 'src/book-display/book-display-widget.tsx'),
             },
             output: {
                 entryFileNames: '[name].js',
-                chunkFileNames: 'chunks/[name]-[hash].js',
-                assetFileNames: 'assets/[name][extname]'
+                assetFileNames: '[name].css',
+                chunkFileNames: '[name].js', // Optional: makes shared chunks readable
             },
         },
+        outDir: 'dist',
+        emptyOutDir: true,
     },
 })
