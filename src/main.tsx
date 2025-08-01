4c2d81b791b4bc8339e1858395773deff114aadf
Ehhthan <ethanhaynes46@gmail.com>
2025-08-01 01:41:32 -0500

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter, Route, Routes} from 'react-router';
+import {HashRouter, Route, Routes} from 'react-router';
 import {BookDisplay, BookDisplayDemo} from "./book-display/book-display.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/widgets/book" element={<BookDisplay text={'Test'} maxPages={2} />} />
               <Route path="/widgets/demo/book" element={<BookDisplayDemo />} />
           </Routes>
-      </BrowserRouter>,
+      </HashRouter>,
   </StrictMode>,
 )
