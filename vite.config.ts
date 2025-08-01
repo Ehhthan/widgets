7f234ee0b358dfbfbc39a32c91ac3e209e2f650d
Ehhthan <ethanhaynes46@gmail.com>
2025-08-01 11:01:21 -0500

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,10 +13,14 @@ export default defineConfig({
     publicDir: 'public',
     build: {
         rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, "src/main.tsx"),
+                bookDisplayWidget: path.resolve(__dirname, "src/book-display/book-display-widget.tsx"),
+            },
             output: {
-                entryFileNames: `index.js`,
-                chunkFileNames: `chunk.js`,
-                assetFileNames: `style.css`,
+                entryFileNames: '[name].js',
+                chunkFileNames: 'chunks/[name]-[hash].js',
+                assetFileNames: 'assets/[name][extname]'
             },
         },
     },
